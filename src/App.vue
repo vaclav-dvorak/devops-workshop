c927015591cbd7d7dff3ec60a3ab568ed2c0b152
vaclav-dvorak <2vd@seznam.cz>
2020-04-10 17:49:04 +0200

    Add Anodina font-face

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,6 @@ body {
   height: 100%;
   width: 100%;
   font-family: Anodina, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
-    Oxygen, Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue",
     sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -37,5 +36,14 @@ body {
   letter-spacing: 0.05em;
   color: rgb(230, 230, 230);
   background: linear-gradient(rgb(85, 85, 85) 0%, rgb(101, 101, 101) 100%);
+}
+
+@font-face {
+  font-family: "Anodina";
+  src: local("Anodina Light"), local("Anodina-Light"),
+    url("./assets/fonts/Anodina-Light.woff2") format("woff2"),
+    url("./assets/fonts/Anodina-Light.woff") format("woff");
+  font-weight: 300;
+  font-style: normal;
 }
 </style>
