c122122150873d483eee832d43c209e942f26320
vaclav-dvorak <2vd@seznam.cz>
2020-04-21 10:25:14 +0200

    Remove autoscaler and small fixes

--- server/helm/install.sh
+++ server/helm/install.sh
@@ -2,6 +2,8 @@
 set -e
 
 helm tiller start-ci
-helm install --name traefik --namespace kube-system --values traefik.yml stable/traefik
-helm install --name workshop-test --namespace workshop-test ./charts/devops-workshop
+
+helm upgrade --install --name traefik --namespace kube-system --values traefik.yml stable/traefik
+helm upgrade --install --namespace devops-workshop-dev --set dockerTag=latest,db.user=rebood_workshop,db.password=WG4NqWzrWgsLtnlI,ingress.hosts={devops-workshop.test.rebood.com} --wait devops-workshop-dev ./charts/devops-workshop
+
 helm tiller stop
